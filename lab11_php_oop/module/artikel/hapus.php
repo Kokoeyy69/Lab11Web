f814f3a5be55c1dd4c1af534cd57a1e0d1e7c960
EnricoSyaffalullahArmandasyah <84768188+Kokoeyy69@users.noreply.github.com>
2025-12-19 06:43:11 +0700

    Delete lab11_php_oop directory

--- lab11_php_oop/module/artikel/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new Database();
-$id = $_GET['id'];
-
-$db->query("DELETE FROM artikel WHERE id=$id");
-
-header("Location: /lab11_php_oop/artikel/index");
-exit;
-?>
--- lab11_php_oop/template/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div class="sidebar">
-    <h3>Menu</h3>
-    <a href="/lab11_php_oop/artikel/index">Artikel</a><br>
-    <a href="/lab11_php_oop/artikel/tambah">Tambah Artikel</a>
-</div>
